260ef2a055ebff6fa8271673c79a3052bcd286f5
Jeevan1809 <harshajeevan22@gmail.com>
2025-10-17 15:06:01 +0530

    Add files via upload

---
+++ myreact/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+      <p>I am Jeevan,From BNG</p>
+    </div>
+  )
+}
+export default App;
